e21327aa3d5f467942ac26ea79d9fa5611f42ce8
Aaditya Kumar Verma <99467531+aadityakumarverma@users.noreply.github.com>
2023-07-31 23:56:58 +0530

    Update HashMap_1.java

--- HashMap_1.java
+++ HashMap_1.java
@@ -5,6 +5,7 @@ public class HashMap_1 {
         HashMap<Integer,String> map= new HashMap<>();
         map.put(19001,"Aaditya");
         map.put(19003,"Aditya");
+        map.put(19004,"Amisha");
         map.put(19007,"Anu");
         map.put(19024,"Jaya");
         map.put(19027,"Mehnaj");
