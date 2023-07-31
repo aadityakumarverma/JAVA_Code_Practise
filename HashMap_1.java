1f278da22be3c08d3fe18b05b2b22f9ba72c2791
aadityakumarverma <raushankv217@gmail.com>
2023-07-31 23:47:57 +0530

    HashMap Created

---
+++ HashMap_1.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class HashMap_1 {
+    public static void main(String[] args) {
+        HashMap<Integer,String> map= new HashMap<>();
+        map.put(19001,"Aaditya");
+        map.put(19003,"Aditya");
+        map.put(19007,"Anu");
+        map.put(19024,"Jaya");
+        map.put(19027,"Mehnaj");
+
+        System.out.println(map);
+    }
+}
